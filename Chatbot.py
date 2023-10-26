7b050c45dab012373a67b3b2e9d82c7eeaab462a
Jakko Turro <jakkoturro@gmail.com>
2023-10-26 22:21:03 +0300

    Bot töötab!

--- Chatbot.py
+++ Chatbot.py
@@ -1,2 +1,17 @@
-import Chatbot
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 
+# Create a new chat bot named Charlie
+chatbot = ChatBot('hi')
+
+trainer = ListTrainer(chatbot)
+
+trainer.train([
+
+])
+
+# Get a response to the input text 'I would like to book a flight.'
+vastus = input()
+response = chatbot.get_response(vastus)
+
+print(response)
