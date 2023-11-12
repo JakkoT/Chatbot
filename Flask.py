ebfcde73a1acb0dc0a92cb0f8053c07ada795005
JakkoT <jakkoturro@gmail.com>
2023-11-12 17:25:28 +0200

    hell yeah history töötab

--- Flask.py
+++ Flask.py
@@ -5,13 +5,26 @@ import Chatbot
 
 app = Flask(__name__)
 
-vastus = ""
-
+vastus = ["Chat History:", "  "]
 
 @app.route("/", methods =["GET", "POST"])
 def html():
-    vastus = ""
-    if request.method == "POST":
-       küs = request.form.get("küs")
-       vastus = Chatbot.vastus(küs)
-    return render_template("index.html", data=vastus)
+   f = open("history.txt", "r")
+   küs = "Hi"
+   vastus = ["Sinu, Ajalugu"]
+   if request.method == "POST":
+      küs = request.form.get("küs")
+   for i in f:
+      vastus.append(i.strip())
+   Cvastus = Chatbot.vastus(küs)
+   vastus.append(küs)
+   vastus.append(Cvastus)
+   f.close()
+   f = open("history.txt","a")
+   f.write(str(küs) + "\n")
+   f.write(str(Cvastus) + "\n")
+   f.close()
+
+
+       
+   return render_template("index.html", data=vastus)
